9a0020985d1ba7ce85aaf6598d00c3c5eea86c9a
anfeela <flysee@vip.qq.com>
2020-01-01 23:14:46 +0800

    day2 homework

---
+++ homework2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys 
+number = 10000
+total = number/50*30
+if total > number :
+    print('????5000')
+else :
+    print('????5000')
+
+
+#!/usr/bin/python
+import sys 
+score = 87.5
+print('????',end='')
+if score > 90 :
+    print('a')
+elif score >= 80 :
+    print('b')
+elif score >= 70 :
+    print('c')
+elif score >= 60 :
+    print('d')
+else :
+    print('e')
